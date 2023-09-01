1aeae3b7896e309983fbb7bb57041ed04581e046
Max Geller <mxgeller@gmail.com>
2023-08-31 18:19:15 -0700

    adding particles.js

---
+++ app/_components/Particles.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
+import styles from "./Particles.module.css";
+
+export default function ParticlesComponent() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+  return (
+    <Particles
+      className={styles.particles}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#0000",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: false,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.8,
+            width: 0.2,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 0.3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 0.1, max: 1 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./_components/About";
 import Contact from "./_components/Contact";
 import Hero from "./_components/Hero";
+import ParticlesComponent from "./_components/Particles";
 import RecentMusic from "./_components/RecentMusic";
 import RecentPhotos from "./_components/RecentPhotos";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pb-24">
+      <ParticlesComponent />
       <Hero />
       <About />
       <RecentPhotos />
